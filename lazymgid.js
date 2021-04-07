d82ff1ed0a57962920d58b80a784b72de4fc1662
pelangiblog <82078182+pelangiblog@users.noreply.github.com>
2021-04-07 12:49:09 +0700

    Add files via upload

---
+++ lazymgid.js
@@ -0,0 +1,5 @@
+(function () {
+var script = document.createElement('script');
+script.src = "//jsc.mgid.com/p/e/pelangiblog.com.1108869.js?t=" +  ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : '');
+script.async = true;
+document.body.appendChild(script);})();
