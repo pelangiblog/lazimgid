045d9a9628d44163e50379a3af76b572cda0ad41
pelangiblog <82078182+pelangiblog@users.noreply.github.com>
2022-02-16 14:44:24 +0700

    Update lazymgid.js

--- lazymgid.js
+++ lazymgid.js
@@ -1,5 +1,5 @@
 (function () {
 var script = document.createElement('script');
-script.src = "//jsc.mgid.com/p/e/pelangiblog.com.1108869.js?t=" +  ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : '');
+script.src = "https://jsc.mgid.com/p/e/pelangiblog.com.1108869.js?t=" +  ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : '');
 script.async = true;
 document.body.appendChild(script);})();
